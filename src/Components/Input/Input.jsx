fa26bc986d20b429e6e51ee76fb503ed11ffee4b
Shivanshu Mishra <93570605+Shaivaan@users.noreply.github.com>
2022-06-24 15:23:38 +0530

    debounced

--- src/Components/Input/Input.jsx
+++ src/Components/Input/Input.jsx
@@ -1,6 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {GoLocation} from "react-icons/go"
+import { SearchCard } from '../SearchCard/SearchCard';
+import {cities} from "../Cities/cities";
+
 export const Input = () => {
+
+  const [show,setShow] = useState(true);
+  const [searchData,setSearchData] = useState([]);
+  
+
+  let id;
+  const changeInput = (inp)=>{
+    setShow(true);
+    if(id){
+      clearTimeout(id);
+    }
+
+       id = setTimeout(()=>{
+       let filterData = [];
+       cities.forEach((el) => {
+          if(el.name.includes(inp)){
+            filterData.push(el);
+          }
+       });
+
+        console.log(filterData);
+       setSearchData([...filterData])
+      },800)
+  }
+
+
+  const onClickCity = ()=>{
+    setShow(false);
+  }
+
   return (
     <div>
         <br />
@@ -8,9 +41,17 @@ export const Input = () => {
             <div>
       <GoLocation className='ii'/>
             </div>
-      <input type="text" id = "inp" placeholder='Search' color='grey'/>
+      <input type="text" id = "inp" placeholder='Search' onChange={(e)=>{changeInput(e.target.value)}} color='grey'/>
         </div>
             
+            {/* {searchData.length == 0 ? "" :   } */}
+          {searchData.length !== 0 && show == true && <div className='optionBox'>
+              {searchData.map((el,i)=>{
+               return <div key={i} onClick= {onClickCity}>
+                 <SearchCard  city= {el.name} state= {el.state}/>
+               </div>
+              })}
+            </div>}
     </div>
   )
 }
---
+++ src/Components/SearchCard/SearchCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const SearchCard=({city,state})=> {
+  return (
+    <>
+    <div className='city'>
+      <h3>ㅤ  {city}, {state} </h3>
+    </div>
+    </>
+  )
+}
+
+
